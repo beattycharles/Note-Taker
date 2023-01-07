a49fb4d2e017d9dc6a87a0b43a68c70e453c77e9
beattycharles <113803621+beattycharles@users.noreply.github.com>
2023-01-07 12:29:15 -0500

    delete is working

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const fs = require("fs");
 const express = require("express");
 const path = require("path");
-const notes = require('./db/db.json')
+let notes = require("./db/db.json");
 const { v4: uuidv4 } = require("uuid");
 
 const PORT = process.env.PORT || 3001;
@@ -15,13 +15,23 @@ app.use(express.static("public"));
 
 // GET
 app.get("/api/notes", (req, res) => {
-  res.send(notes);
-  console.info(`${req.method} has been received`);
+   fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+        res.json(err)
+      } else {
+        const parsedNotes = JSON.parse(data);
+        console.info(`${req.method} has been received`);
+        notes = parsedNotes
+        res.json(parsedNotes);
+      }
+    })
+
 });
 
 // POST
 app.post("/api/notes", (req, res) => {
-  res.json(`${req.method} has been received`);
+// res.json(`${req.method} has been received`);
   console.info(`${req.method} has been received`);
   const { title, text } = req.body;
   if (title && text) {
@@ -30,23 +40,38 @@ app.post("/api/notes", (req, res) => {
       text,
       id: uuidv4(),
     };
-    fs.readFile("./db/db.json", "utf8", (err, data) => {
-      if (err) {
-        console.error(err);
-      } else {
-        const parsedNotes = JSON.parse(data);
-        parsedNotes.push(newNote);
+    //put read and write into there own func.
+    // fs.readFile("./db/db.json", "utf8", (err, data) => {
+    //   if (err) {
+    //     console.error(err);
+    //   } else {
+    //     const parsedNotes = JSON.parse(data);
+        notes.push(newNote);
         fs.writeFile(
           "./db/db.json",
-          JSON.stringify(parsedNotes, null, 4),
-          (writeErr) => (writeErr ? console.error(writeErr) : res.send())
+          JSON.stringify(notes, null, 4),
+          (writeErr) => (writeErr ? res.json(err) : res.json(notes))
         );
-      }
-    });
+       // res.json(parsedNotes)
+      //}
+   // });
   } else {
     res.status(500).json("Error in adding notes");
   }
 });
+app.delete("/api/notes/:id",(req,res) => {
+  let notbeingReadded = []
+  for (let i = 0; i < notes.length; i++) {
+    if ( notes[i].id != req.params.id){
+      notbeingReadded.push(notes[i])
+    }
+    
+  }
+  notes = notbeingReadded
+   fs.writeFile("./db/db.json", JSON.stringify(notes, null, 4), (writeErr) =>
+     writeErr ? res.json(err) : res.json(notes)
+   );
+})
 
 //redirects to either index or notes
 app.get("/notes", (req, res) =>
